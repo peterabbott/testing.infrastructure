1beb3960b66a05f8ac58c80ac73af99b7aaf421f
peter.abbott <peter.abbott@akqa.com>
2015-03-11 20:02:33 +1300

    update java version on centos

--- test/integration/default/serverspec/test_java_spec.rb
+++ test/integration/default/serverspec/test_java_spec.rb
@@ -3,7 +3,7 @@ require 'serverspec'
 set :backend, :exec
 
 describe command('java -version'), :if => os[:family] == 'redhat' do
-  its(:stdout) { should match /1.7.0_71/ }
+  its(:stdout) { should match /1.7.0_75/ }
 end
 
 describe command('java -version'), :if => os[:family] == 'ubuntu' do
